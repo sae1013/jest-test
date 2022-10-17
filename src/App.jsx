f5a7ee5fbb5055958652872b6cae17ff223516c1
sae1013 <sae1013@gmail.com>
2022-10-18 00:56:59 +0900

    비동기 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React,{ useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import axios from 'axios';
 import './App.css'
+import ScoopOptions from "./components/entry/ScoopOptions.jsx";
+import Option from "./components/entry/Option.jsx";
 
 function App() {
   useEffect(()=>{
     axios.get('http://localhost:3030/test').then(res=>console.log(res.data))
   },[])
   return (
-    <div className="App" role={"main"}>
+    <div className="App">
       {/*<button role="tab" aria-selected={"true"}>React</button>*/}
       <button>submit</button>
+      <Option optionType={"scoops"}/>
     </div>
   )
 }
---
+++ src/test/entry/Option.test.js
@@ -0,0 +1,13 @@
+import {render,screen,waitFor,waitForElement} from "@testing-library/react";
+import {act} from 'react-dom/test-utils';
+
+import Option from '../../components/entry/Option.jsx';
+
+test("Vite Jest test",async()=> {
+  render(<Option optionType={"scoops"}/>);
+  //find Images
+  const scoopImages = await screen.findAllByRole('img',
+    {name:/scoop$/i}) ;
+  expect(scoopImages).toHaveLength(2);
+
+})
