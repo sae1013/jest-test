c22feefd699d2c7303aba8bfda801841f480193e
sae1013 <sae1013@gmail.com>
2022-10-18 23:10:14 +0900

    Context Api 통합테스트

--- src/test/entry/Option.test.js
+++ src/test/entry/Option.test.js
@@ -1,13 +1,13 @@
-import {render,screen,waitFor,waitForElement} from "@testing-library/react";
+import {render, screen, waitFor, waitForElement} from "@testing-library/react";
 import {act} from 'react-dom/test-utils';
 
 import Option from '../../components/entry/Option.jsx';
 
-test("Vite Jest test",async()=> {
+test("Vite Jest test", async () => {
   render(<Option optionType={"scoops"}/>);
   //find Images
   const scoopImages = await screen.findAllByRole('img',
-    {name:/scoop$/i}) ;
+    {name: /scoop$/i});
   expect(scoopImages).toHaveLength(2);
   // const error = screen.getByRole('error');
   // expect(error).toBeInTheDocument();
