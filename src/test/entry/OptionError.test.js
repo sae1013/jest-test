1b48762a06fe2ac3ca843296422751ba4fd29620
sae1013 <sae1013@gmail.com>
2022-10-18 14:04:28 +0900

    handler 오버라이드 && 에러 테스트

--- src/test/entry/OptionError.test.js
+++ src/test/entry/OptionError.test.js
@@ -3,7 +3,23 @@ import {server} from "../../mocks/server.js";
 import {rest} from 'msw'
 import ScoopOptions from "../../components/entry/ScoopOptions.jsx";
 import Option from '../../components/entry/Option.jsx'
-test('http Reqeust Error Test', async () => {
+
+test.only('http Reqeust Error Test', async () => {
+  server.resetHandlers(
+    rest.get('http://localhost:3000/scoops', (req, res, ctx) => {
+      return res(ctx.status(500),ctx.json({
+          message:'something went wrong'
+        })
+      )
+    }),
+  )
+  render(<Option optionType={"scoops"}></Option>)
+  const alertModal = await screen.findByRole('error-alert')
+  expect(alertModal).toBeInTheDocument();
+
+})
+
+  test.only('http Reqeust Error Test', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3000/scoops', (req, res, ctx) => {
       return res(ctx.status(500),ctx.json({
